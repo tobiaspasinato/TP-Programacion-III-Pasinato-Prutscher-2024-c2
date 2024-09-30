445514d1c9de915db265a40ba0dbb81f5ab0d84f
tobiaspasinato <tobiasmarpasinato@gmail.com>
2024-09-30 12:06:49 -0300

    nioqui suprem final

--- frontend/LoginAdmin/scripts_login_admin.js
+++ frontend/LoginAdmin/scripts_login_admin.js
@@ -1,3 +1,33 @@
 document.getElementById('btnBack').addEventListener('click', () => {
     window.location.href = '../index.html'; // Redirige a la página de inicio
 });
+
+// Alterna entre tema claro y oscuro
+const themeToggleBtn = document.getElementById('themeToggleBtn');
+themeToggleBtn.addEventListener('click', () => {
+    const body = document.body;
+
+    // Alterna entre las clases 'light' y 'dark'
+    if (body.classList.contains('light')) {
+        body.classList.remove('light');
+        body.classList.add('dark');
+    } else {
+        body.classList.remove('dark');
+        body.classList.add('light');
+    }
+
+    // Animación del botón
+    themeToggleBtn.style.transform = 'rotate(360deg)';
+    setTimeout(() => {
+        themeToggleBtn.style.transform = '';
+    }, 300);
+});
+
+// Al cargar la página, aplicamos el tema oscuro por defecto
+document.body.classList.add('dark');
+
+// Agrega el ícono del sol al botón al cargar la página
+const iconMoon = document.createElement('span');
+iconMoon.classList.add('icon-moon');
+iconMoon.innerHTML = '&#9728;'; // Ícono de sol
+themeToggleBtn.appendChild(iconMoon);
