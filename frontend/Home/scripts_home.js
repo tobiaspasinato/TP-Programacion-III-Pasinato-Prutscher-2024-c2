54b2b967c887fdf7de5fd1f445f1ffd1ac72c87f
Matías Nicolás Prutscher <matiasprutscher@gmail.com>
2024-10-02 16:14:21 -0300

    se actualiza el home
    falta solucionar problema con el cambio de tema claro/oscuro y agregar grilla de productos

---
+++ frontend/Home/scripts_home.js
@@ -0,0 +1,40 @@
+
+const expand_btn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+  document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+
+const allLinks = document.querySelectorAll(".sidebar-links a")
+
+allLinks.forEach((elem) => {
+  elem.addEventListener('click', function() {
+    const hrefLinkClick = elem.href;
+
+    allLinks.forEach((link) => {
+      if (link.href == hrefLinkClick){
+        link.classList.add("active");
+      } else {
+        link.classList.remove('active');
+      }
+    });
+  })
+});
+const themeToggleBtn = document.getElementById("themeToggleBtn");
+
+themeToggleBtn.addEventListener("click", () => {
+  // Alterna entre las clases 'dark' y 'light' en el cuerpo del documento
+  if (document.body.classList.contains("dark")) {
+    document.body.classList.remove("dark");
+    document.body.classList.add("light");
+  } else {
+    document.body.classList.remove("light");
+    document.body.classList.add("dark");
+  }
+});
+
+
