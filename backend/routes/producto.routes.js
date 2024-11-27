1b5ffe3a9165959f8d63fcef31c1a812c660feea
tobiaspasinato <tobiasmarpasinato@gmail.com>
2024-11-27 02:15:53 -0300

    1231

--- backend/routes/producto.routes.js
+++ backend/routes/producto.routes.js
@@ -7,12 +7,20 @@ router.get("/", (req, res) => {
     res.send("test: Estas en producto");
 });
 
+router.delete("/delete/:id", comprobarID, async (req, res) => {
+    const resultado = productoSequelize.update(
+        { eliminado: true },
+        { where: { id: req.params.id } }
+    );
+    res.send('Producto "eliminado"!');
+});
+
 router.post("/insert", async (req, res) => {
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const imagen = req.body.imagen;
+    const tipo = req.body.tipo;
     try {
-        const nombre = req.body.nombre;
-        const precio = req.body.precio;
-        const imagen = req.body.imagen;
-        const tipo = req.body.tipo;
         const resultado = await productoSequelize.create({
             nombre: nombre,
             precio: precio,
@@ -25,9 +33,14 @@ router.post("/insert", async (req, res) => {
     }
 });
 
-router.get('/:id', async (request, response) => {
-    const resultado = await productoSequelize.findByPk(request.params.id);
+router.get('/:id', comprobarID, async (request, response) => {
+    const resultado = await productoSequelize.findByPk(request.params.id, { where: { eliminado: false } });
     response.status(200).send(resultado);
+});
+
+router.get("/list", async (req, res) => {
+    const resultado = await productoSequelize.findAll({ where: { eliminado: false } });
+    res.status(200).send(resultado);
 });
 
 module.exports = router;
