5fa45295c5dc3e3224ec6f186de91662f1afc977
tobiaspasinato <tobiasmarpasinato@gmail.com>
2024-12-01 16:48:31 -0300

    bug ultra instinto

--- backend/routes/producto.routes.js
+++ backend/routes/producto.routes.js
@@ -12,7 +12,39 @@ router.get("/", (req, res) => {
     res.send("test: Estas en producto");
 });
 
-router.delete("/delete/:id", comprobarID, async (req, res) => {
+router.patch("/update/:id", async (req, res) => {
+    const nombre = req.body.nombre;
+    const precio = req.body.precio;
+    const tipo = req.body.tipo;
+    console.log(req.body);
+    try {
+        const resultado = await productoSequelize.update(
+            {
+                nombre: nombre,
+                precio: precio,
+                tipo: tipo
+            },
+            { where: { id: req.params.id } }
+        );
+        console.log(resultado);
+        res.status(200).send("Producto actualizado!");
+    } catch (error) {
+        res.status(404).send(`ERROR: ${error}`);
+    }
+});
+
+router.get("/listar", async (req, res) => {
+    try {
+        const resultado = await productoSequelize.findAll();
+        res.render("producto", { productos: resultado });
+    } catch (error) {
+        res.status(404).send(`ERROR: ${error}`);
+    }
+    const resultado = await productoSequelize.findAll();
+    res.render("producto", { productos: resultado });
+});
+
+router.delete("/delete/:id", async (req, res) => {
     const resultado = productoSequelize.update(
         { eliminado: true },
         { where: { id: req.params.id } }
@@ -20,7 +52,7 @@ router.delete("/delete/:id", comprobarID, async (req, res) => {
     res.send('Producto "eliminado"!');
 });
 
-router.delete("/borrar/:id", comprobarID, async (req, res) => {
+router.delete("/borrar/:id", async (req, res) => {
     const resultado = productoSequelize.destroy(
         { where: { id: req.params.id } }
     );
@@ -45,40 +77,9 @@ router.post("/insert", upload.single("image"), async (req, res) => {
     }
 });
 
-router.put("/update/:id", comprobarID, async (req, res) => {
-    const nombre = req.body.nombre;
-    const precio = req.body.precio;
-    const tipo = req.body.tipo;
-    try {
-        const resultado = await productoSequelize.update(
-            {
-                nombre: nombre,
-                precio: precio,
-                tipo: tipo
-            },
-            { where: { id: req.params.id } }
-        );
-        console.log(resultado);
-        res.status(200).send("Producto actualizado!");
-    } catch (error) {
-        res.status(404).send(`ERROR: ${error}`);
-    }
-});
-
-router.get('/:id', comprobarID, async (request, response) => {
-    const resultado = await productoSequelize.findByPk(request.params.id, { where: { eliminado: false } });
+router.get('/:id', async (request, response) => {
+    const resultado = await productoSequelize.findByPk(request.params.id);
     response.status(200).send(resultado);
-});
-
-router.get("/list", async (request, response) => {
-    console.log("Listar productos");
-    try {
-        const lista_productos = await productoSequelize.findAll();
-        response.status(501).render("productos", { productos: lista_productos });
-    } catch (error) {
-        console.error("Error al listar productos:", error);
-        response.status(500).send("Error al listar productos");
-    }
 });
 
 module.exports = router;
