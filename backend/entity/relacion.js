92c52d064f48247f42ae77691e63ea1ea997db55
tobiaspasinato <tobiasmarpasinato@gmail.com>
2024-12-01 15:24:59 -0300

    asd

--- backend/entity/relacion.js
+++
@@ -1,9 +0,0 @@
-const producto = require('./productoEntity.js');
-const ventas = require('./ventasEntity.js');
-
-function relacionarEntidades(){
-    producto.belongsToMany(Estacion, {through: 'TrenEstacion'});
-    ventas.belongsToMany(Tren, {through: 'TrenEstacion'});
-}
-
-module.exports = relacionarEntidades;
